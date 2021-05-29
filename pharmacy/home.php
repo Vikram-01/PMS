64f63e0801d4ffb7b3f5f9daafb33c02aca97dad
Vikram-01 <61109050+Vikram-01@users.noreply.github.com>
2021-05-29 18:11:01 +0530

    Add files via upload

---
+++ pharmacy/home.php
@@ -0,0 +1,158 @@
+<html>
+<?php
+session_start();
+?>
+	<head>
+		<style>
+			
+			#title{
+				background-color:#ffa500;
+				font-size:33px;
+				
+				
+				color:white;
+				margin-left:20px;
+				margin-top:20px;
+				margin-bottom:20px;
+				
+				}
+				
+			ul {
+			list-style-type: none;
+			margin: 0;
+			padding: 0;
+			overflow: hidden;
+			background-color: #ffa500;
+			}
+			
+			li {
+				float: right;
+			}
+			
+			#titlehead{
+				float: left;
+			}
+
+			li a {
+				display: block;
+				color: white;
+				font-size:20px;
+				text-align: center;
+				padding: 16px 20px;
+				margin-top:10px;
+				text-decoration: none;
+			}
+
+			li a:hover:not(.active) {
+				background-color: #FF0000;
+			}
+
+			.active {
+				background-color: #FF0000;
+			}
+			
+			#home_img{
+				padding-left:50px;
+				padding-bottom:10px;
+				
+			}
+			
+			#bottom_posts{
+				
+				display: grid;
+				grid-template-columns: auto auto auto;
+				padding: 10px;
+			
+			}
+			
+			#img_title{
+				
+				display: grid;
+				grid-template-columns: auto auto auto;
+				padding: 5px;
+			
+			}
+			
+			#posts{
+				padding: 20px;
+				font-size: 30px;
+				text-align: center;
+			
+			}
+			
+		</style>
+	</head>
+	
+	<body>
+		
+		
+		
+		<ul>
+			<li id="titlehead"><p id="title">Pharmacy</p></li>
+			<li style=margin-right:10px;><a href="admin_login.php">Admin</a></li>
+			<li><a href="pharmacist_login.php">Pharmacist</a></li>
+			<li><a href="user_login.php">User</a></li>
+			<li><a class="active" href="home.php">Home</a></li>
+		</ul>
+		
+		<div id=bottom_posts>
+		
+			<div id=posts>
+				<h2 style="font-size:28px;"> Pharmacy Info </h2>
+				<div id=img_title>
+					<img src="image\pharmacy.png">
+					<p style="font-size:20px;"> Easy access the pharmacy details management</p>
+				
+				</div>
+			
+			</div>
+			
+			<div id=posts>
+				<h2 style="font-size:28px;"> Medicine Details</h2>
+				<div id=img_title>
+					<img src="image\medicinemain.png">
+					<p style="font-size:20px;">Medicines can be added and updated easily</p>
+				
+				</div>
+			</div>
+			
+			<div id=posts>
+				<h2 style="font-size:28px;"> Easy Access for Users </h2>
+				<div id=img_title>
+					<img src="image\searchimg.png">
+					<p style="font-size:20px;">Any medicines can be accessed in few seconds</p>
+				
+				</div>
+			</div>
+		
+		</div>
+		
+		
+		
+		
+		<?php
+		
+		$servername = "localhost";
+		$username = "root";
+		$password = "";
+
+		
+		$conn = new mysqli($servername, $username, $password);
+	
+		if ($conn->connect_error) {
+			die("Connection failed: " . $conn->connect_error);
+		} 
+
+		$sql = "CREATE DATABASE IF NOT EXISTS pharmacydb";
+		if ($conn->query($sql) === TRUE) {
+			
+			
+		
+		
+		$conn->close();
+		}
+		?>
+		
+	</body>
+	
+</html>
